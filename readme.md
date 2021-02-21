a9817031c1e920e703c593d8876c382c75a6074f
erislash <eri.gim2008@gmail.com>
2021-02-20 21:49:03 -0300

    Without Desktop Version

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Daily IU #1 - Sign Up
+
+## Design Hint
+
+Create a sign up page, modal, form, or app screen related to signing up for something. It could be for a volunteer event, contest registration, a giveaway, or anything you can image.
+
+## Design trend
+I decided to use Neumorphism design to make today's challenge. I visited a few websites for inspiration, such as:
+* Dribble
+  - [Freebie Neumorphic UX UI Elements](https://dribbble.com/shots/9527558-Freebie-Neumorphic-UX-UI-Elements)
+  - [Mobile app - Tracker](https://dribbble.com/shots/13911945-Mobile-app-Tracker)
+  - [Tesla Smart App](https://dribbble.com/shots/10196092-Tesla-Smart-App)
+  - [Simple Music Player](https://dribbble.com/shots/9338617-Simple-Music-Player)
+  - [Music UI dark & light](https://dribbble.com/shots/11244469-Music-UI-dark-light)
+* Pinterest
+* And the most important, the [**Neumorphism UI**](https://www.google.com) UI kit website
+
+
+
+## Usage
+
+```python
+import foobar
+
+foobar.pluralize('word') # returns 'words'
+foobar.pluralize('goose') # returns 'geese'
+foobar.singularize('phenomena') # returns 'phenomenon'
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', StartApp);
+
+function StartApp() {
+    const scene = document.querySelector('#scene');
+    const home = document.querySelector('#home');
+    const signIn = document.querySelector('#signIn');
+    const signUp = document.querySelector('#signUp');
+
+    const signInBtn = document.querySelector('#signInBtn');
+    const signUpBtn = document.querySelector('#signUpBtn');
+
+    const notRegisteredBtn = document.querySelector('#notRegisteredBtn');
+    const alreadyAccountBtn = document.querySelector('#alreadyAccountBtn');
+
+
+    scene.dataset.state = 'home';
+    console.log(scene.dataset.state);
+    ShowScene({current: scene.dataset.state, home, signIn, signUp});
+
+console.log(notRegisteredBtn);
+
+    signInBtn.addEventListener('click', function(e){ HandleSceneChange.call(this, e,'signIn')});
+    signUpBtn.addEventListener('click', function(e){ HandleSceneChange.call(this, e,'signUp')});
+    notRegisteredBtn.addEventListener('click', function(e){ HandleSceneChange.call(this, e,'signUp')});
+    alreadyAccountBtn.addEventListener('click', function(e){ HandleSceneChange.call(this, e,'signIn')});
+    
+    
+}
+
+function ShowScene({current, home, signIn, signUp}) {
+    home.style.display = home.id == current ? 'flex' : 'none';
+    signIn.style.display = signIn.id == current ? 'flex' : 'none';
+    signUp.style.display = signUp.id == current ? 'flex' : 'none';
+}
+
+function HandleSceneChange(e, newScene){
+        const scene = document.querySelector('#scene');
+        scene.dataset.state = newScene;
+        ShowScene({current: scene.dataset.state, home, signIn, signUp});
+    
+}
